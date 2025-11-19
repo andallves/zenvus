1da9b592cc01ba7ee57e00555bd362444939f3c1
Andre Alves <andrealves10a@gmail.com>
2025-11-19 11:27:11 -0300

    ZEN-57 feat: implementando sidebar

--- ui/src/app/shared/layouts/sidenav/menu-item/menu-item.component.ts
+++ ui/src/app/shared/layouts/sidenav/menu-item/menu-item.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { SidenavMenuItems } from '../nav-data';
+import {NgClass} from '@angular/common';
+import {Component, Input} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {SidenavMenuItems} from '../nav-data';
 
 @Component({
   selector: 'zen-sidenav-menu-item',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './menu-item.component.html',
   styleUrl: './menu-item.component.scss',
 })
 export class MenuItemComponent {
+  @Input({ required: true }) isActiveBar!: boolean;
   @Input() label!: string;
   @Input() path!: string;
   @Input() classIcon!: string;
