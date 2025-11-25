9c6b7ee1acec66195f6c2048e943e2e2d1b99f8a
Andre Alves <andrealves10a@gmail.com>
2025-11-25 17:17:40 -0300

    ZEN-57 refactor: ajustando sidebar

--- ui/src/app/shared/layouts/default-layout/components/sidenav-header/sidenav-header.component.ts
+++ ui/src/app/shared/layouts/default-layout/components/sidenav-header/sidenav-header.component.ts
@@ -1,11 +1,18 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgClass, NgOptimizedImage } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { SidebarService } from '@core/services/sidebar/sidebar.service';
 
 @Component({
   selector: 'zen-sidenav-header',
   standalone: true,
   templateUrl: './sidenav-header.component.html',
   styleUrl: './sidenav-header.component.scss',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, NgClass],
 })
-export class SidenavHeaderComponent {}
+export class SidenavHeaderComponent {
+  private readonly sidebarService = inject(SidebarService);
+
+  get isActiveSidebar(): boolean {
+    return this.sidebarService.isActive;
+  }
+}
