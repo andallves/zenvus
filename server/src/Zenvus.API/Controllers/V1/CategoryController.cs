c30fd468b6f066d8f21128099db9d886bf9901d7
Andre Alves <andrealves10a@gmail.com>
2025-11-27 23:30:54 -0300

    ZEN-50 feat: implementando EnableCategoryCommand.cs

--- server/src/Zenvus.API/Controllers/V1/CategoryController.cs
+++ server/src/Zenvus.API/Controllers/V1/CategoryController.cs
@@ -60,4 +60,15 @@ public class CategoryController(IMediator mediator) : BaseController(mediator)
         }
         return await SendCommandAsync(command, cancellationToken);
     }
+    
+    [HttpPatch("{id:int}/enable")]
+    [MapToApiVersion("1.0")]
+    [SwaggerOperation(Summary = "Habilita um curso existente", Tags = ["Categoria"])]
+    [ProducesResponseType(typeof(CategoryDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async ValueTask<IActionResult> Enable([FromRoute] int id, CancellationToken cancellationToken)
+    {
+        return await SendCommandAsync(new EnableCategoryCommand { Id = id }, cancellationToken);
+    }
 }                         
---
+++ server/src/Zenvus.Application/Commands/Categories/EnableCategoryCommand.cs
@@ -0,0 +1,10 @@
+using MediatR;
+using Zenvus.Application.DTO.Category;
+using Zenvus.Core.ValueObjects;
+
+namespace Zenvus.Application.Commands.Categories;
+
+public class EnableCategoryCommand : BaseCommand<CategoryDto>
+{
+    public int Id { get; set; }
+}
