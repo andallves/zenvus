c4acdd8927f4933b6299947939b360c535756133
Andre Alves <andrealves10a@gmail.com>
2026-01-20 19:27:02 -0300

    ci test

--- ui/src/environments/environment.production.ts
+++ ui/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  env: 'production',
-  production: false,
+  env: 'prod',
+  production: true,
   apiUrl: 'https://api.zenvus.com.br',
   noticiasApiUrl: 'https://api.zenvus.com.br/v1',
 };
